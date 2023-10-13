fe04adcd98d988f3045ab7f04ee22fc2e1605fbf
Savan Patoliya <savanpatoliya987@gmail.com>
2023-10-13 12:15:07 +0530

    background color change in signup

--- client/src/components/auth/Signup.jsx
+++ client/src/components/auth/Signup.jsx
@@ -98,48 +98,49 @@ const Signup = () => {
 
   return (
     <>
-      <div className='w-full pt-10 px-4 md:px-8 lg:px-16 flex flex-col items-center'>
-        {/* Title */}
-        <div className='mb-4 text-center'>
-          <span className='text-black text-3xl md:text-4xl lg:text-5xl font-semibold'>
-            Welcome to{' '}
-          </span>
-          <span className='text-emerald-400 text-3xl md:text-4xl lg:text-5xl font-semibold'>
-            Umlimi!
-          </span>
-        </div>
-
-        <div className='pt-3 w-full md:w-[60%] lg:w-[40%] '>
-          {/* Name Fields */}
-          <div className='flex  md:flex-row gap-5 md:gap-8'>
-            <div className='w-[100%]'>
-              <div className='text-zinc-900 text-lg md:text-xl lg:text-2xl '>
-                First Name
-              </div>
-              <input
-                type='text'
-                name='first_name'
-                className='bg-white w-full rounded border-2 border-gray-300 p-2 mt-2 h-12 px-3'
-                placeholder='Mehrab'
-                onChange={(e) => setFirstName(e.target.value)}
-              />
-            </div>
-            <div className='w-[100%]'>
-              <div className='text-zinc-900 text-lg md:text-xl lg:text-2xl '>
-                Last Name
-              </div>
-              <input
-                type='text'
-                name='last_name'
-                className='bg-white w-full rounded border-2 border-gray-300 p-2 mt-2 h-12 px-3'
-                placeholder='Bozorgi'
-                onChange={(e) => setLastName(e.target.value)}
-              />
-            </div>
+      <div className='bg-emerald-100 h-[100vh] w-[100vw]'>
+        <div className='w-full pt-10 px-4 md:px-8 lg:px-16 flex flex-col items-center'>
+          {/* Title */}
+          <div className='mb-4 text-center'>
+            <span className='text-black text-3xl md:text-4xl lg:text-5xl font-semibold'>
+              Welcome to{' '}
+            </span>
+            <span className='text-emerald-400 text-3xl md:text-4xl lg:text-5xl font-semibold'>
+              Umlimi!
+            </span>
           </div>
 
-          {/* Email */}
-          {/* <div className='pt-3'>
+          <div className='pt-3 w-full md:w-[60%] lg:w-[40%] '>
+            {/* Name Fields */}
+            <div className='flex  md:flex-row gap-5 md:gap-8'>
+              <div className='w-[100%]'>
+                <div className='text-zinc-900 text-lg md:text-xl lg:text-2xl '>
+                  First Name
+                </div>
+                <input
+                  type='text'
+                  name='first_name'
+                  className='bg-white w-full rounded border-2 border-gray-300 p-2 mt-2 h-12 px-3'
+                  placeholder='Mehrab'
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </div>
+              <div className='w-[100%]'>
+                <div className='text-zinc-900 text-lg md:text-xl lg:text-2xl '>
+                  Last Name
+                </div>
+                <input
+                  type='text'
+                  name='last_name'
+                  className='bg-white w-full rounded border-2 border-gray-300 p-2 mt-2 h-12 px-3'
+                  placeholder='Bozorgi'
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </div>
+            </div>
+
+            {/* Email */}
+            {/* <div className='pt-3'>
             <div className='text-zinc-900 text-lg md:text-xl lg:text-2xl '>
               Email
             </div>
@@ -163,48 +164,50 @@ const Signup = () => {
               </span>
             )}
           </div> */}
-          <div className='pt-3'>
-            <div className='text-zinc-900 text-lg md:text-xl lg:text-2xl'>
-              Email
-            </div>
-            <input
-              type='email'
-              name='email'
-              disabled={otp && true}
-              className='bg-white w-[80%] rounded border border-zinc-500 h-12 px-3'
-              placeholder='Mehrabbozorgi.business@gmail.com'
-              value={email}
-              onChange={validateEmail}
-            />
-            {email && (
-              <button
-                type='button'
-                className=' px-4 py-2'
-                onClick={() => {
-                  openModal()
-                  handleSendOTP()
-                }}
-                style={{ color: '#2929d1' }}
-              >
-                Verify Email
-              </button>
-            )}
+            <div className='pt-3'>
+              <div className='text-zinc-900 text-lg md:text-xl lg:text-2xl'>
+                Email
+              </div>
+              <input
+                type='email'
+                name='email'
+                disabled={otp && true}
+                className='bg-white w-[80%] rounded border border-zinc-500 h-12 px-3'
+                placeholder='Mehrabbozorgi.business@gmail.com'
+                value={email}
+                onChange={validateEmail}
+              />
+              {email && (
+                <button
+                  type='button'
+                  className=' px-4 py-2'
+                  onClick={() => {
+                    openModal()
+                    handleSendOTP()
+                  }}
+                  style={{ color: '#2929d1' }}
+                >
+                  Verify Email
+                </button>
+              )}
 
-            {isModalOpen && (
-              <PopUp_Dialog onClose={() => setIsModalOpen(false)} />
+              {isModalOpen && (
+                <PopUp_Dialog onClose={() => setIsModalOpen(false)} />
+              )}
+            </div>
+            {email && (
+              <span
+                className={
+                  emailError === 'Valid Email'
+                    ? 'text-green-500'
+                    : 'text-red-500'
+                }
+              >
+                {emailError}
+              </span>
             )}
-          </div>
-          {email && (
-            <span
-              className={
-                emailError === 'Valid Email' ? 'text-green-500' : 'text-red-500'
-              }
-            >
-              {emailError}
-            </span>
-          )}
-          {otp && <span className='text-green-500'>Email verified</span>}
-          {/* {email && (
+            {otp && <span className='text-green-500'>Email verified</span>}
+            {/* {email && (
             <div className='flex gap-5'>
               <button
                 className='bg-white rounded-sm border h-9 px-3 text-emerald-400 border-emerald-400 hover:cursor-pointer'
@@ -224,60 +227,60 @@ const Signup = () => {
             </div>
           )} */}
 
-          {/* Password */}
-          <div className='pt-3'>
-            <div className='text-zinc-900 text-lg md:text-xl lg:text-2xl '>
-              Set Password
+            {/* Password */}
+            <div className='pt-3'>
+              <div className='text-zinc-900 text-lg md:text-xl lg:text-2xl '>
+                Set Password
+              </div>
+              <input
+                type='password'
+                name='password'
+                value={password}
+                className='bg-white w-full rounded border-2 border-gray-300 p-2 mt-2 h-12 px-3'
+                placeholder='Enter your password'
+                onChange={validatePassword}
+              />
+              {password && (
+                <span
+                  className={
+                    passwordError === 'Strong Password'
+                      ? 'text-green-500'
+                      : 'text-red-500'
+                  }
+                >
+                  {passwordError}
+                </span>
+              )}
             </div>
-            <input
-              type='password'
-              name='password'
-              value={password}
-              className='bg-white w-full rounded border-2 border-gray-300 p-2 mt-2 h-12 px-3'
-              placeholder='Enter your password'
-              onChange={validatePassword}
-            />
-            {password && (
-              <span
-                className={
-                  passwordError === 'Strong Password'
-                    ? 'text-green-500'
-                    : 'text-red-500'
-                }
-              >
-                {passwordError}
-              </span>
-            )}
-          </div>
 
-          {/* Confirm Password */}
-          <div className='pt-3'>
-            <div className='text-zinc-900 text-lg md:text-xl lg:text-2xl '>
-              Confirm Password
+            {/* Confirm Password */}
+            <div className='pt-3'>
+              <div className='text-zinc-900 text-lg md:text-xl lg:text-2xl '>
+                Confirm Password
+              </div>
+              <input
+                type='password'
+                name='confirm_password'
+                value={confirmPassword}
+                className='bg-white w-full rounded border-2 border-gray-300 p-2 mt-2 h-12 px-3'
+                placeholder='Confirm your password'
+                onChange={handleConfirmPassword}
+              />
+              {confirmPassword && (
+                <span
+                  className={
+                    confirmPasswordError === 'Passwords match'
+                      ? 'text-green-500'
+                      : 'text-red-500'
+                  }
+                >
+                  {confirmPasswordError}
+                </span>
+              )}
             </div>
-            <input
-              type='password'
-              name='confirm_password'
-              value={confirmPassword}
-              className='bg-white w-full rounded border-2 border-gray-300 p-2 mt-2 h-12 px-3'
-              placeholder='Confirm your password'
-              onChange={handleConfirmPassword}
-            />
-            {confirmPassword && (
-              <span
-                className={
-                  confirmPasswordError === 'Passwords match'
-                    ? 'text-green-500'
-                    : 'text-red-500'
-                }
-              >
-                {confirmPasswordError}
-              </span>
-            )}
-          </div>
 
-          {/* Gender and DOB */}
-          {/* <div className="flex flex-col md:flex-row md:gap-8 pt-3">
+            {/* Gender and DOB */}
+            {/* <div className="flex flex-col md:flex-row md:gap-8 pt-3">
             <div className="w-full">
               <div className="text-zinc-900 text-lg md:text-xl lg:text-2xl font-semibold">
                 Gender
@@ -314,43 +317,43 @@ const Signup = () => {
             </div>
           </div> */}
 
-          {/* Buttons */}
-          <div className='mt-6 flex flex-col md:flex-row md:justify-center md:items-center gap-4'>
-            <button
-              type='button'
-              className='bg-white rounded-sm border border-emerald-400 w-full md:w-32 lg:w-40 hover:cursor-pointer'
-              onClick={handleReset}
-            >
-              <div className='text-emerald-400 text-lg md:text-xl lg:text-2xl font-normal p-2'>
-                Reset
-              </div>
-            </button>
-            <button
-              type='button'
-              className='bg-emerald-400 rounded-sm w-full md:w-32 lg:w-40 hover:cursor-pointer'
-              onClick={handleRegister}
-            >
-              <div className='text-white text-lg md:text-xl lg:text-2xl font-semibold p-2'>
-                Register
-              </div>
-            </button>
-          </div>
+            {/* Buttons */}
+            <div className='mt-6 flex flex-col md:flex-row md:justify-center md:items-center gap-4'>
+              <button
+                type='button'
+                className='bg-white rounded-sm border border-emerald-400 w-full md:w-32 lg:w-40 hover:cursor-pointer'
+                onClick={handleReset}
+              >
+                <div className='text-emerald-400 text-lg md:text-xl lg:text-2xl font-normal p-2'>
+                  Reset
+                </div>
+              </button>
+              <button
+                type='button'
+                className='bg-emerald-400 rounded-sm w-full md:w-32 lg:w-40 hover:cursor-pointer'
+                onClick={handleRegister}
+              >
+                <div className='text-white text-lg md:text-xl lg:text-2xl font-semibold p-2'>
+                  Register
+                </div>
+              </button>
+            </div>
 
-          {/* Login Prompt */}
-          <div className='mt-4 text-center'>
-            <span className='text-black text-lg md:text-xl lg:text-2xl font-semibold'>
-              Already have an account?{' '}
-            </span>
-            <span
-              className='text-emerald-400 text-lg md:text-xl lg:text-2xl font-semibold hover:cursor-pointer'
-              onClick={() => navigateTo('/login')}
-            >
-              Log in
-            </span>
+            {/* Login Prompt */}
+            <div className='mt-4 text-center'>
+              <span className='text-black text-lg md:text-xl lg:text-2xl font-semibold'>
+                Already have an account?{' '}
+              </span>
+              <span
+                className='text-emerald-400 text-lg md:text-xl lg:text-2xl font-semibold hover:cursor-pointer'
+                onClick={() => navigateTo('/login')}
+              >
+                Log in
+              </span>
+            </div>
           </div>
         </div>
       </div>
-
       {/* Footer */}
       {/* <div className='pt-10 flex justify-center'>
         <svg
